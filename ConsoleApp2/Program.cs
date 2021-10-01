8f610bcde8f3094baedbd9055e5a0fe06d9f2056
MezemovRufetCode <rufatam@code.edu.az>
2021-10-01 18:55:27 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] p = { 1, 2, 3, 4, 5, 6,-5,-4,0,0};
+            Task1Algo t1 = new Task1Algo(p);
+            t1.Find1(p);
+        }
+    }
+}
---
+++ ConsoleApp2/Task1Algo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Task1Algo
+    {
+        public int[] Arr;
+        public Task1Algo(int[] arr)
+        {
+            Arr=arr;
+        }
+        public void Find1(int[] array)
+        {           
+            int countmin = 0;
+            int countplus = 0;
+            int countzero = 0;
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == 0)
+                {
+                    countzero++;
+                }
+                else
+                if (array[i] %2== 0)
+                {
+                    countplus++;
+                }
+                else
+                    countmin++;
+            }
+            Console.WriteLine($"Count of zero: {countzero}\nCount of negative: {countmin}\nCount of positive: {countplus}");
+
+        }
+    }
+}
